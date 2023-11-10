4a7aff566edc2f335353bb57f1354303609411a7
Archie Amparado <61049475+Ace-tech01@users.noreply.github.com>
2023-11-10 10:56:28 +0800

    added sidebar components, finish dashboard page

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
-  <ul>
+  <!-- <ul>
     <li><router-link :to="{name:'home'}">Home</router-link></li>
     <li><router-link :to="{name:'products'}">Product</router-link></li>
     <li><router-link :to="{name:'admin'}">Admin</router-link></li>
     <li><router-link :to="{name:'orders'}">Order</router-link></li>
   </ul>
-  <router-view/> 
+  <router-view/>  -->
+  <Order/>
 </template>
 
 <script setup>
 import {RouterView, RouterLink} from 'vue-router'
+import Order from './pages/Order.vue';
 </script>
