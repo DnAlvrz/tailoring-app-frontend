0770269d2fcee42aabdb3b9dd880b95adf836375
Archie Amparado <61049475+Ace-tech01@users.noreply.github.com>
2023-12-22 13:01:14 +0800

    added register page

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,10 @@
 <template>
   <div>
     <!-- <SidebarVue/> -->
-    <Login/>
   </div>
-   <!-- <RouterView/> -->
+   <RouterView/>
 </template>
 
 <script setup>
-import Login from './pages/Login.vue';
 import SidebarVue from './components/Sidebar.vue';
 </script>
---
+++ src/pages/Register.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="min-h-screen bg-primary-0 flex flex-col">
+    <LoginRegisterNav/>
+      <main class="flex-1 flex items-center md:items-start justify-center">
+        <div class="p-8 md:w-auto max-md:max-w-lg w-full">
+            <CardRegister/>
+        </div>
+      </main>
+    <Footer/>
+    </div>
+  </template>
+  
+  <script setup>
+import LoginRegisterNav from '../components/customer/nav/Login.Register.Nav.vue';
+import Footer from '../components/customer/footer/Footer.vue';
+import CardRegister from '../components/customer/card/Card.Register.vue';
+  </script>
