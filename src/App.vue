f271789f0d56b5ae13c15e157abc739b6a33b215
Archie Amparado <61049475+Ace-tech01@users.noreply.github.com>
2023-12-22 12:55:29 +0800

    added login page

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <div>
     <!-- <SidebarVue/> -->
+    <Login/>
   </div>
-   <RouterView/>
+   <!-- <RouterView/> -->
 </template>
 
 <script setup>
+import Login from './pages/Login.vue';
 import SidebarVue from './components/Sidebar.vue';
 </script>
---
+++ src/pages/Login.vue
@@ -0,0 +1,18 @@
+@ -1,53 +1,17 @@
+<template>
+  <div class="min-h-screen bg-primary-0 flex flex-col">
+    <LoginRegisterNav/>
+    <main class="flex-1 flex md:mt-16 items-center md:items-start justify-center">
+      <div class="p-8 md:w-auto max-md:max-w-lg w-full">
+        <CardLogin/>
+      </div>
+    </main>
+    <Footer/>
+  </div>
+</template>
+
+<script setup>
+import LoginRegisterNav from '../components/customer/nav/Login.Register.Nav.vue';
+import Footer from '../components/customer/footer/Footer.vue';
+import CardLogin from '../components/customer/card/Card.Login.vue';
+</script>
