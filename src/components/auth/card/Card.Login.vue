db53b340d100a9f997fd837ce4e01f793f50ae2c
Don Alvarez <mintchip00073@gmail.com>
2024-01-01 17:07:04 +0800

    feature: user login

---
+++ src/components/auth/card/Card.Login.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="flex w-full shadow border rounded-3xl">
+        <!-- 1st content -->
+        <div class="hidden md:flex justify-center bg-tertiary-0 rounded-l-3xl items-center">
+            <img class="p-8 md:w-[25rem] lg:w-[40rem] max-w-full" src="../../../assets/Login.svg" alt="">
+        </div>
+        <!-- 2nd content -->
+        <div class="w-full px-5 md:w-1/2 py-6 md:px-10">
+            <div class="text-center mb-10">
+                <h1 class="font-bold text-3xl text-secondary-0 font-roboto">LOGIN</h1>
+            </div>
+            <div class="mb-6">
+                <label for="email" class="block mb-2 text-sm font-medium text-secondary-0">Email address</label>
+                <input  v-model="credentials.username" type="email" id="email" class="bg-gray-50 border border-gray-300 text-secondary-0 text-sm rounded-lg focus:ring-tertiary-0 focus:border-tertiary-0 block w-full p-2.5" placeholder="john.doe@gmail.com" required>
+            </div> 
+            <div class="mb-6">
+                <label for="password" class="block mb-2 text-sm font-medium text-secondary-0">Password</label>
+                <input v-model="credentials.password" type="password" id="password" class="bg-gray-50 border border-gray-300 text-secondary-0 text-sm rounded-lg focus:ring-tertiary-0 focus:border-tertiary-0 block w-full p-2.5" placeholder="•••••••••" required>
+            </div>
+            <!-- <div class="mb-6 text-center p-4 border-b text-blue-500 hover:underline">
+                <a href="">Forgot Password?</a>
+            </div> -->
+            <div class="text-center mb-6">
+                <span>Doesn't have an account? <br> <a class="text-blue-500 hover:underline" href="">Sign Up</a></span>
+            </div>
+            <button @click="login()" class="text-white bg-tertiary-0 hover:bg-secondary-0 font-medium rounded-lg text-sm w-full sm:w-full px-5 py-2.5 text-center">LOGIN</button>
+
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+const emit = defineEmits(['login']);
+
+const credentials = ref({   
+    username: "",
+    password: ""
+});
+
+function login() {
+    emit('login', credentials.value)
+}
+</script>
--- src/pages/Auth/Login.vue
+++ src/pages/Auth/Login.vue
@@ -1,10 +1,10 @@
-@ -1,53 +1,17 @@
+
 <template>
   <div class="min-h-screen bg-primary-0 flex flex-col">
     <LoginRegisterNav/>
     <main class="flex-1 flex md:mt-16 items-center md:items-start justify-center">
       <div class="p-8 md:w-auto max-md:max-w-lg w-full">
-        <CardLogin/>
+        <CardLogin @login="login" />
       </div>
     </main>
     <Footer/>
@@ -12,7 +12,48 @@
 </template>
 
 <script setup>
-import LoginRegisterNav from '../components/customer/nav/Login.Register.Nav.vue';
-import Footer from '../components/customer/footer/Footer.vue';
-import CardLogin from '../components/customer/card/Card.Login.vue';
+import axios from 'axios';
+import {ref} from 'vue';
+import LoginRegisterNav from '@/components/customer/nav/Login.Register.Nav.vue';
+import Footer from '@/components/customer/footer/Footer.vue';
+import CardLogin from '@/components/auth/card/Card.Login.vue';
+
+const isLoading = ref(false)
+
+const login = async (credentials) => {
+  try {
+    isLoading.value = true;
+    const data = {
+        grant_type: 'password',
+        client_id: import.meta.env.VITE_CLIENT_ID,
+        client_secret: import.meta.env.VITE_CLIENT_SECRET,
+        ...credentials
+    }
+    const response = await axios.post(`${import.meta.env.VITE_BACKEND_AUTH}`, data);
+    if (response.status === 200 && response.data  && response.data.access_token) {
+    
+      const config = {
+        headers: {
+          Authorization: `Bearer ${response.data.access_token}` 
+        }
+      }
+
+      const userData = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/user`, config);
+
+      localStorage.setItem('user',JSON.stringify({
+        id: userData.data.id,
+        name: userData.data.first_name +" " +userData.data.last_name,
+        token: response.data.access_token,
+        access_level: userData.data.access_level ? userData.data.access_level : 2
+      }));  
+    }
+  } catch (error) {
+    console.log(error)
+  }
+  finally {
+    isLoading.value = false;
+  }
+
+}
+
 </script>
