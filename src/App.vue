be9b0cce354c28b0d37c9482b086971c236c3e27
Archie Amparado <61049475+Ace-tech01@users.noreply.github.com>
2024-01-06 23:03:56 +0800

    added dropdown to the cart button

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <div>
+    <Home/>
     <!-- <SidebarVue/> -->
   </div>
-   <RouterView/>
+   <!-- <RouterView/> -->
 </template>
 
 <script setup>
+import Home from './pages/Home.vue';
 import SidebarVue from './components/Sidebar.vue';
 </script>
