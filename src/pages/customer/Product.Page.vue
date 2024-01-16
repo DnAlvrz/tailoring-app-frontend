a7df3a8e4f7d95c622f24bc2695a0938d09dbe78
Don Alvarez <mintchip00073@gmail.com>
2024-01-16 08:54:51 +0800

    fixed: carousel not working

--- src/pages/customer/Product.Page.vue
+++ src/pages/customer/Product.Page.vue
@@ -5,59 +5,8 @@
             <div class="rounded-lg md:w-2/3">
                 <div v-if="!isLoading">
                     <div class="border-b lg:flex lg:flex-row bg-primary-0 border-gray-300">
-                        <div class=" flex h-60 lg:h-auto lg:w-[70%] overflow-hidden" href="#">
-                            <div id="indicators-carousel" class="relative w-full" data-carousel="static">
-                                <!-- Carousel wrapper -->
-                                <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
-                                    <!-- Item 1 -->
-                                    <div class="hidden duration-700 ease-in-out" data-carousel-item="active">
-                                        <img :src="imagesUrl+'/'+product.images[0].name" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
-                                    </div>
-                                    <!-- Item 2 -->
-                                    <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                                        <img src="/docs/images/carousel/carousel-2.svg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
-                                    </div>
-                                    <!-- Item 3 -->
-                                    <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                                        <img src="/docs/images/carousel/carousel-3.svg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
-                                    </div>
-                                    <!-- Item 4 -->
-                                    <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                                        <img src="/docs/images/carousel/carousel-4.svg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
-                                    </div>
-                                    <!-- Item 5 -->
-                                    <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                                        <img src="/docs/images/carousel/carousel-5.svg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
-                                    </div>
-                                </div>
-                                <!-- Slider indicators -->
-                                <div class="absolute z-30 flex -translate-x-1/2 space-x-3 rtl:space-x-reverse bottom-5 left-1/2">
-                                    <button type="button" class="w-3 h-3 rounded-full" aria-current="true" aria-label="Slide 1" data-carousel-slide-to="0"></button>
-                                    <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
-                                    <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
-                                    <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 4" data-carousel-slide-to="3"></button>
-                                    <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 5" data-carousel-slide-to="4"></button>
-                                </div>
-                                <!-- Slider controls -->
-                                <button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
-                                    <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                                        <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
-                                        </svg>
-                                        <span class="sr-only">Previous</span>
-                                    </span>
-                                </button>
-                                <button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
-                                    <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                                        <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
-                                        </svg>
-                                        <span class="sr-only">Next</span>
-                                    </span>
-                                </button>
-                            </div>
-
-                            <!-- <img class="object-fill w-full" :src="imagesUrl+'/'+product.images[0].name" alt="product image" /> -->
+                        <div class="h-60 lg:h-auto lg:w-[70%] overflow-hidden">
+                            <fwb-carousel slide  :pictures="productImages" />
                         </div>
                         <div class="p-4">
                             <h2 class="font-roboto font-semibold lg:text-2xl text-secondary-0 line-clamp-2">{{ product.name }}</h2>
@@ -189,6 +138,7 @@
 </template>
 <script setup>
 import Navbar from '@/components/Navbar.vue';
+import { FwbCarousel } from 'flowbite-vue'
 import { useRoute } from "vue-router"
 import ProductFooter from '../../components/customer/footer/Product.Footer.vue';
 import Footer from '../../components/customer/footer/Footer.vue';
@@ -196,13 +146,14 @@ import axios from 'axios'
  import { onMounted, ref } from 'vue'
  import { 
     initDropdowns, 
-     initModals,
+    initModals,
  } from 'flowbite'
 const imagesUrl = ref(import.meta.env.VITE_IMAGES_URL);
 const route = useRoute()
 const isLoading = ref(true);
 const product = ref(null);
 const quantity= ref(1);
+const productImages = ref([]);
 
 const addToCart = async () => {
     const cart = JSON.parse(localStorage.getItem("cart")) || [];
@@ -225,6 +176,7 @@ const addToCart = async () => {
     })
     localStorage.setItem("cart", JSON.stringify(cart));
 }
+
 const getProduct = async () => {
     try {
         isLoading.value = true;
@@ -236,16 +188,19 @@ const getProduct = async () => {
         };
         const res = await axios.get(`${backendUrl}/products/${route.params.id}`, config)
         product.value = res.data.product
+        productImages.value = product.value.images.map((image) => { return { src: imagesUrl.value + '/' + image.name } })
     } catch (error) {
         console.log(error)
     } finally {
         isLoading.value = false;
     }
 }
+
  // initialize components based on data attribute selectors
  onMounted(() => {
-     initDropdowns();
-     initModals();
-     getProduct();
+    initDropdowns();
+    initModals();
+    getProduct();
+    
  })
 </script>
