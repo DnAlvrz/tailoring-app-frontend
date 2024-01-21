44b9edafbc373efbd821cf0263e53f3ca0784095
Archie Amparado <61049475+Ace-tech01@users.noreply.github.com>
2024-01-22 04:48:00 +0800

    none

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,10 @@
 <template>
   <div>
-    <EditRating/>
     <!-- <SidebarVue/> -->
   </div>
    <!-- <RouterView/> -->
 </template>
 
 <script setup>
-import EditRating from './pages/customer/Edit.Rating.vue';
 import SidebarVue from './components/Sidebar.vue';
 </script>
