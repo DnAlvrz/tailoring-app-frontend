a0185dce264d02a293b61fbfab541a75a5066026
Archie Amparado <61049475+Ace-tech01@users.noreply.github.com>
2024-01-22 03:31:16 +0800

    added rating page

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <div>
+    <Rating/>
     <!-- <SidebarVue/> -->
   </div>
-   <RouterView/>
+   <!-- <RouterView/> -->
 </template>
 
 <script setup>
+import Rating from './pages/customer/Rating.vue';
 import SidebarVue from './components/Sidebar.vue';
 </script>
